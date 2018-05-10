6b8021d6e0c8accb5ea5ce5450e6daacced57c32
Antonella Solomon <antonellasolomon@gmail.com>
2018-05-10 13:11:13 -0400

    added another line

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,6 +2,7 @@
 public class MainApp {
 
 	public static void main(String[] args) {
+		System.out.println("This is a new addition!");
 		System.out.println("Hello World!");
 		System.out.println("Hello My name is Maurice");
 
